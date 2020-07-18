356dd91751c8ab73d8a957c07f2a0b14292d4519
Julio Raphael <jraphaeloliveira14@gmail.com>
2020-07-18 16:57:25 -0300

    Adjustments and bug fixes.

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject aplicationIcons;
+    public GameObject galleryIcons;
+    private GameObject blink;
+
+    private bool isAplication = true;
+
+    private void InvertAplication(bool isAplication)
+    {
+        aplicationIcons.SetActive(isAplication);
+        galleryIcons.SetActive(!isAplication);
+    }
+
+    public void ApplicationExit()
+    {
+        if (isAplication)
+            Application.Quit();
+        
+        else 
+            InvertAplication(true);
+    }
+
+    public void GalleryButton()
+    {
+        InvertAplication(false);
+    }
+
+    public void ScreenShootButton() 
+    {
+        StartCoroutine(this.CaptureIt());
+    }
+
+    IEnumerator CaptureIt() 
+    {
+
+        print("Isso ta pegando");
+        string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+        string filename = "Screenshot" + timeStamp + ".png";
+        string pathToSave = filename;
+        ScreenCapture.CaptureScreenshot(pathToSave);
+        yield return new WaitForEndOfFrame();
+    
+    }
+}
